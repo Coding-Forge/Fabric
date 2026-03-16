26741ff5c37b45a55e288f5e1d1c952282b46211
BrandonCampbell <brandon.campbell@microsoft.com>
2026-03-16 13:44:41 -0400

    updated trigger for 2 am and updated python packages in requirements

--- function_app.py
+++ function_app.py
@@ -5,8 +5,10 @@ from app.monitor import main as Monitor
 
 app = func.FunctionApp()
 
-# the function app runs at UTC time so you need to padd the time to your local time
-@app.schedule(schedule="0 0 */4 * * *", arg_name="myTimer", run_on_startup=True,
+# Runs once daily at 02:00 UTC. Azure Functions uses 6-field NCRONTAB:
+# {second} {minute} {hour} {day} {month} {day-of-week}
+# To change the time, adjust the hour field (e.g. "0 0 6 * * *" = 6:00 AM UTC)
+@app.schedule(schedule="0 0 2 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def Fabric_Monitor(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
